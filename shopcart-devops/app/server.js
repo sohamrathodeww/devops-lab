db8708cc1cec6aa75261857a086be833918b1071
sohamrathodeww <96477546+sohamrathodeww@users.noreply.github.com>
2026-06-30 12:38:29 +0000

    Ticket0-01 Initial Express setup

---
+++ shopcart-devops/app/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+    res.send("🚀 ShopCart API Running");
+
+});
+
+app.listen(PORT, () => {
+
+    console.log("=================================");
+    console.log(`${process.env.APP_NAME}`);
+    console.log(`Server Started`);
+    console.log(`Environment : ${process.env.NODE_ENV}`);
+    console.log(`Port : ${PORT}`);
+    console.log("=================================");
+
+});
